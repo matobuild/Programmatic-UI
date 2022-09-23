385052e2c4540fe06f39e09f3fd6935af66697b6
Mato <tissuepaperarelimited@gmail.com>
2022-09-23 22:50:07 +0800

    Initial Commit

---
+++ Programmatic-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic-UI
+//
+//  Created by kittawat phuangsombat on 2022/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
